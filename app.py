39af39acfbf39ca40f224208ff17486a1c45c670
Cian O'Gralaigh <ihatecian@gmail.com>
2020-08-11 00:40:33 +0100

    Add functionality for listening to user and performing google search if user asks

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import speech_recognition as sr
+import webbrowser
 from os import path
 
 
@@ -6,11 +7,26 @@ from os import path
 r = sr.Recognizer()
 
 
-def print_audiofile():
-    with sr.AudioFile("./audio/speech.wav") as source:
+def record_audio(ask=False):
+    with sr.Microphone() as source:
+        if ask:
+            print(ask)
         audio = r.listen(source)
         voice_data = ''
+        try:
+            voice_data = r.recognize_google(audio)
+            print(voice_data)
+        except sr.UnknownValueError:
+            print('Try talking without eating')
+        except sr.RequestError:
+            print('Get better internet')
+        return voice_data
 
+
+def print_audiofile(audiofile):
+    with sr.AudioFile(audiofile) as source:
+        audio = r.listen(source)
+        voice_data = ''
         try:
             voice_data = r.recognize_google(audio)
             print(voice_data)
@@ -26,6 +42,11 @@ def respond(audio):
         print('shut up Smeagol')
     if ('potatoes' in audio):
         print('Whats "taters", eh?')
+    if ('search' in audio):
+        search = record_audio('What shall we search for Preciousss?')
+        url = 'https://google.com/search?q=' + search
+        webbrowser.get().open(url)
+        print('Precious, I foundss this for you...')
 
 
-respond(print_audiofile())
+respond(record_audio())
